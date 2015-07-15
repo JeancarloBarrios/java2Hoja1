e7b4bd98d750d9460f5b91e7a8326e46a624fa5a
Jeancarlo Barrios <jeancarlobarrios@gmail.com>
2015-07-15 08:13:32 -0600

    Change RADIO name to Radio

---
+++ Radio.java
@@ -0,0 +1,22 @@
+
+
+
+public interface Radio {
+	
+	public void setESTADO(boolean ESTADO);
+	
+	public boolean getEstado();
+	
+	public int getAMFM();
+	
+	public void setAMFM(int Band);
+	
+	public void Sintonizar(boolean ud);
+	
+	public void Guardar(int Pos);
+	
+	public void Memoria(int Pos);
+	
+	public double getEmisora();
+	
+}
